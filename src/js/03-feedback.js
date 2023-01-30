e0774b180b8a11e57169e9c69b5c7e20b4e3b903
Olha Savva <106154412+OlhaSavva@users.noreply.github.com>
2023-01-30 15:58:52 +0200

    new

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,22 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const STORAGE_KEY = "feedback-msg";
+const STORAGE_KEY = 'feedback-msg';
 const feedbackForm = {};
 let savedMessage = {};
 
-const formEl = document.querySelector(".feedback-form");
-formEl.addEventListener("submit", onFormSubmit);
-formEl.addEventListener("input", throttle(handleFormInput), 500);
-populateInput();
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(handleFormInput), 500);
 
 function handleFormInput(e) {
   feedbackForm[e.target.name] = e.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackForm));
-  localStorage.getItem(STORAGE_KEY);
 }
 
-function populateInput() {
+if (localStorage.getItem(STORAGE_KEY)) {
   savedMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
-  console.log(savedMessage);
-  if (savedMessage) {
-    formEl.email.value = savedMessage.email;
-    formEl.message.value = savedMessage.message;
+  for (let key in savedMessage) {
+    formEl.elements[key].value = savedMessage[key];
   }
 }
 
@@ -28,4 +24,7 @@ function onFormSubmit(event) {
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(STORAGE_KEY);
+  if (savedMessage) {
+    console.log(savedMessage);
+  }
 }
